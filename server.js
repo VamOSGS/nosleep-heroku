b01291067477ee12fe2ed198eda12177422bbba5
Gegham Samvelyan <vamosgs2001@gmail.com>
2017-11-23 23:29:07 +0400

    Removing sites ready

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Koa = require('koa');
 const Router = require('koa-router');
 const koaBody = require('koa-body');
+const cors = require('@koa/cors');
 const app = new Koa();
 const router = new Router();
 const port = process.env.PORT || 3000;
@@ -14,8 +15,12 @@ app.listen(port, () => {
     console.log(`listening on port: ${port}`)
 })
 
+app
+    .use(cors())
+    .use(koaBody())
+    .use(router.routes())
+
 router.get('/', async(ctx, next) => {
-    console.log(sites);
     ctx.body = sites;
 })
 
@@ -23,22 +28,22 @@ router.post('/sites', async(ctx, next) => {
     const data = ctx.request.body;
     let newSites = sites;
     const validate = newSites.list.indexOf(data.name) == -1;
-    newSites.list = [...newSites.list, data.name];
-    
+
     if (validate) {
-        fs.writeFile('./sites.json', JSON.stringify(newSites), (err) => {
-            if (err) throw err;
-        });
-        ctx.body = 'sites.json has been changed';
+        newSites.list = [...newSites.list, data.name];
+        updateFile(newSites);
+        ctx.body = newSites;
     } else {
         ctx.body = 'Site already exist in list';
     }
-
 })
-
-app
-    .use(koaBody())
-    .use(router.routes())
+router.delete('/sites/:name', async(ctx, next) => {
+    const name = ctx.params.name;
+    let newSites = sites;
+    newSites.list = newSites.list.filter(item => item !== name);
+    updateFile(newSites);
+    ctx.body = newSites;
+})
 
 
 readSitesAndSend();
@@ -56,4 +61,10 @@ function readSitesAndSend() {
 
 function minsToMiliSecs(mins) {
     return mins * 6e4;
+}
+
+function updateFile(content) {
+    fs.writeFile('./sites.json', JSON.stringify(content), (err) => {
+        if (err) throw err;
+    });
 }
