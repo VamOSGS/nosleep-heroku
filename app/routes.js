8f478ffc3c5c05e94e923601c662155b1169472f
Gegham Samvelyan <vamosgs2001@gmail.com>
2017-11-29 20:51:32 +0400

    Get route for admin panel

--- app/routes.js
+++ app/routes.js
@@ -11,23 +11,27 @@ module.exports = (router, sites, render) => {
     }
     return ctx.render('../views/index.hbs')
   })
-
+  router.get('/get', async(ctx, next) => {
+    ctx.body = {
+      sites: sites.list
+    }
+  })
   router.post('/sites/:name', async(ctx, next) => {
     const name = ctx.params.name;
-    console.log('------------------------------------');
-    console.log(name);
-    console.log('------------------------------------');
     let newSites = sites;
     const validate = newSites.list.indexOf(name) == -1;
-
     if (validate) {
       newSites.list = [...newSites.list, name];
       updateFile(newSites);
       ctx.body = newSites;
     } else {
-      ctx.body = 'Site already exist in list';
+      ctx.body = {
+        message:  'Site already exist in list',
+        error: true
+      };
     }
   })
+  
   router.delete('/sites/:name', async(ctx, next) => {
     const name = ctx.params.name;
     let newSites = sites;
